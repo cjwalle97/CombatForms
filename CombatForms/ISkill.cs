18b81331cf7c6e860f399a333f30d0c545439d47
cjwalle97 <cjwalle97@gmail.com>
2017-02-03 11:42:00 -0600

    Update
    Update

---
+++ CombatForms/ISkill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatForms
+{
+    interface IDamagable
+    {
+        void Damage(float);
+    }
+    interface ISkill
+    {
+        void Name(string);
+        void Damage(int);
+    }
+}
---
+++ CombatForms/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatForms
+{
+    public enum Element
+    {
+        0 = Fire,
+        1 = Ice,
+        2 = Electricity,
+        3 = Force,
+        4 = Dark,
+        5 = Light,
+        6 = Slash,
+        7 = Pierce,
+        8 = Bash,
+    }
+    class Player : IDamagable
+    {
+        private int Health;
+        private int Mana;
+        private int Strength;
+        private int Magic;
+        private int Dexterity;
+        private int Agility;
+        private int Luck;
+        private Element Weakness;
+        private Element Resistance;
+
+
+    }
+}
